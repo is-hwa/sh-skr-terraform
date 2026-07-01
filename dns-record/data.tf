54cc84be95216ade8e95bfc8abeffde975927ca2
is-hwa <is-hwa@naver.com>
2026-07-01 17:17:33 +0900

    Delete dns-record directory

--- dns-record/data.tf
+++
@@ -1,4 +0,0 @@
-data "azurerm_resource_group" "rg" {
-  for_each = toset([for r in var.private_dns_records : r.resource_group_name])
-  name     = each.key
-}
--- dns-record/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "private_dns_records" {
-  type = map(object({
-    resource_group_name = string
-    dns_zone_name        = string
-    name                 = string
-    ttl                  = number
-    records              = list(string)
-  }))
-}
