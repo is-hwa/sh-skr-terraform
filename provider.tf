c6cc3f92877ca7a67b2d76a089c8df4fcbb4b2b3
is-hwa <is-hwa@naver.com>
2026-07-16 10:32:02 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,4 +10,6 @@ terraform {
 
 provider "azurerm" {
   features {}
+  # 복수의 구독에 대해 배포를 해야되는 환경에서는 관리 그룹으로 service connection을 만들고 작업마다 subscription id를 지정해야함
+  # subscription_id = var.subscription_id
 }
