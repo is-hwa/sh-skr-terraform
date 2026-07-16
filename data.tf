83e3bb4af4b4dca0980e650bead6ffecbc3b2752
is-hwa <is-hwa@naver.com>
2026-07-16 10:31:28 +0900

    Update resource group and subnet data sources

--- data.tf
+++ data.tf
@@ -1,4 +1,15 @@
 data "azurerm_resource_group" "rg" {
-  for_each = toset([for r in var.private_dns_records : r.resource_group_name])
+  for_each = toset([for k, v in var.storage_accounts : v.resource_group_name])
   name     = each.key
+}
+
+data "azurerm_subnet" "subnet" {
+  for_each = {
+    for k, v in var.storage_accounts : k => v
+    if v.subnet_name != "" && v.vnet_name != ""
+  }
+
+  name                 = each.value.subnet_name
+  virtual_network_name = each.value.vnet_name
+  resource_group_name  = data.azurerm_resource_group.rg[each.value.resource_group_name].name
 }
